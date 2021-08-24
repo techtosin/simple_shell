b7845dad109b26a55fc37cd11c5e540ef6c9b1bc
Olayemi5 <Olayemistephen007@gmail.com>
2021-08-24 07:52:21 -0700

    Finished up file code

--- utility.c
+++ utility.c
@@ -22,3 +22,33 @@ int envir(char *line[] __attribute__((unused)))
         free(line);
         return (1);
 }
+
+/**
+ * maker - a function which excutes the /bin/ls program to list files
+ * @line: an array of command and arguments
+ *
+ * Return: returns 1 in success and -1 if it fails
+ */
+
+int maker(char *line[])
+{
+        int status;
+        pid_t waiter, child;
+        
+        child = fork();
+        if (child == -1)
+                perror("Forking");
+        
+        if (child == 0)
+        {
+                execve("/bin/mkdir", line, NULL);
+        }
+        else
+        {
+                waiter = wait(&status);
+                if (waiter == -1)
+                        perror("Waiting");
+        }
+        free(line);
+        return (1);
+}
